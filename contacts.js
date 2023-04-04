1e838286ee8eab5c18329c998d819e584ee67cd4
Oleksandr Deoneha <oleksandr.deoneha@gmail.com>
2023-04-04 23:09:20 +0300

    start

---
+++ contacts.js
@@ -0,0 +1,42 @@
+const fs = require("fs").promises;
+const path = require("path").promises;
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return Object.values(JSON.parse(data));
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: uuidv4(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
